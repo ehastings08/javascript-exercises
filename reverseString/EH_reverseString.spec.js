0fa18f65b95603d068406617b6bbb3e9e90b5f74
Ellie Hastings <elliehastings@gmail.com>
2018-01-19 08:56:26 -0500

    Add no string test

--- reverseString/EH_reverseString.spec.js
+++ reverseString/EH_reverseString.spec.js
@@ -4,4 +4,8 @@ describe('Reverse String', function() {
   it('reverses a regular string', function() {
     expect(reverseString('hello')).toEqual('olleh');
   });
+
+  it('returns nothing when an empty string is entered', function() {
+    expect(reverseString('')).toEqual('');
+  });
 })
