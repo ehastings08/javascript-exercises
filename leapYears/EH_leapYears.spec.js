afb9d4411f4f6f6e9f6da6b68c0fcc380e4d7201
Ellie Hastings <elliehastings@gmail.com>
2018-01-19 17:27:46 -0500

    Add basic test for non leap year

--- leapYears/EH_leapYears.spec.js
+++ leapYears/EH_leapYears.spec.js
@@ -4,4 +4,8 @@ describe("Leap Years", function() {
   it('correctly identifies a leap year divisible by 4', function() {
     expect(leapYears(1984)).toEqual(true);
   });
+
+  it('correctly rejects an odd numbered non-leap-year', function() {
+    expect(leapYears(1985)).toEqual(false);
+  });
 });
