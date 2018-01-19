168be2c59524938c814d5c145e50f10c2c3a0265
Ellie Hastings <elliehastings@gmail.com>
2018-01-19 13:07:21 -0500

    Add 0 and negative number tests to ftoc

--- tempConversion/EH_tempConversion.spec.js
+++ tempConversion/EH_tempConversion.spec.js
@@ -4,4 +4,12 @@ describe('ftoc', function() {
   it('converts 32F to 0C', function() {
     expect(tempConversion.ftoc(32)).toEqual(0);
   });
+
+  it('converts 0F to -17.78C', function() {
+    expect(+(tempConversion.ftoc(0).toFixed(2))).toEqual(-17.78);
+  });
+
+  it('converts -10F to -23.33C', function() {
+    expect(+(tempConversion.ftoc(-10).toFixed(2))).toEqual(-23.33);
+  });
 });
