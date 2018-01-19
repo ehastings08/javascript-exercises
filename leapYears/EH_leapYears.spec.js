5834e3eb69f9bb1c55dd27b97070767f33fe712a
Ellie Hastings <elliehastings@gmail.com>
2018-01-19 17:29:45 -0500

    Add century years handling

--- leapYears/EH_leapYears.spec.js
+++ leapYears/EH_leapYears.spec.js
@@ -8,4 +8,13 @@ describe("Leap Years", function() {
   it('correctly rejects an odd numbered non-leap-year', function() {
     expect(leapYears(1985)).toEqual(false);
   });
+
+  it('correctly rejects a year divisible by 100 but not 400', function() {
+    expect(leapYears(1800)).toEqual(false);
+  });
+
+  it('correctly accepts a century year divisible by 4', function() {
+    expect(leapYears(2000)).toEqual(true);
+  });
+
 });
