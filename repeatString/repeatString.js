57b49b792ae2b6a4c7cbd647be189122a5a149fe
Ellie Hastings <elliehastings@gmail.com>
2018-01-19 09:01:13 -0500

    Get function working

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-var repeatString = function() {
-
+var repeatString = function(str, num) {
+  if (num >= 0) {
+    var concat_str = '';
+    for (i = 0; i<num; i++) {
+      concat_str += str;
+    }
+    return concat_str;
+  } else {
+    return 'ERROR';
+  }
 }
+
 
 module.exports = repeatString
