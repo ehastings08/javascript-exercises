724ed295ca05bcfb482c38a4947d1d1e24652aae
Ellie Hastings <elliehastings@gmail.com>
2018-01-19 13:19:32 -0500

    Add sumAll and tests

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,13 @@
-var sumAll = function() {
-
+var sumAll = function(min, max) {
+  if (min > max) {
+    return 'Invalid input!';
+  } else {
+    var sum = 0;
+    for (var i=min;i<=max;i++) {
+      sum += i;
+    }
+    return sum;
+  }
 }
 
 module.exports = sumAll
