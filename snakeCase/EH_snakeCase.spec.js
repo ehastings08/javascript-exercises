7dbca5c59c92f22ed2dfb440ff8bc472c20de917
Ellie <elliehastings@gmail.com>
2018-01-20 12:19:48 -0500

    Add second case for camel case

--- snakeCase/EH_snakeCase.spec.js
+++ snakeCase/EH_snakeCase.spec.js
@@ -1,7 +1,12 @@
 var snakeCase = require("./snakeCase");
 
+
 describe("Snake Case", function() {
   it("converts a basic two word phrase into snake case", function() {
     expect(snakeCase('Hello, World!')).toEqual("hello_world");
+  });
+
+  it("converts a compound camelCase word into snake case", function() {
+    expect(snakeCase("snakeCase")).toEqual("snake_case");
   });
 });
--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,12 +1,37 @@
 var snakeCase = function(str) {
-  // First, strip punctuation from string and lowercase it
-  str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
-  // Next, add space before upper case letters
-  // Then, split on spaces
-  split_str = str.split(" ");
-  // Rejoin with underscores
-  snake_case = split_str.join("_");
-  return snake_case;
+  // First, strip punctuation from string
+  str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  // Next, add space before upper case letters IF THERE ARE UPPER CASE LETTERS BUT NO SPACES IN THE STRING. To do this, loop through the string starting at index 1 (we ignore initial capitalization) until an uppercase letter is found. When that happens, splice in a space.
+
+  if (!(hasWhiteSpace(str)) && hasUpperCaseLetters(str)) {
+    var spaced_str = str[0];
+
+    for (var i=1; i<str.length; i++) {
+      if (str[i] == str[i].toUpperCase()) {
+        spaced_str += " "; 
+      }
+      spaced_str += str[i]
+    }
+  } else {
+    spaced_str = str;
+  }
+
+
+  // Next, lower case the string, split on spaces, and rejoin with underscores
+  return spaced_str.toLowerCase().split(" ").join("_");
+}
+
+var hasWhiteSpace = function(str) {
+  return /\s/g.test(str);
+}
+
+var hasUpperCaseLetters = function(str) {
+  for (var i=0; i<str.length; i++) {
+    if (str[i] == str[i].toUpperCase()) {
+      return true;
+    }
+  }
+  return false;
 }
 
 module.exports = snakeCase
